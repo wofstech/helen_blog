7be30e43ce43c1c9a7ff4bd564e70bced20cda25
wofstech <gabrielabuka3@gmail.com>
2018-03-04 08:56:38 -0700

    'adde keep file'

--- helen/settings.py
+++ helen/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 CRISPY_TEMPLATE_PACK="bootstrap3"
 
